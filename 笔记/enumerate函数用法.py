638c1716f3eb664bec9b071f450dfce9406a8b02
tooweakchen <1292095940@qq.com>
2015-10-01 20:13:36 +0800

    enumerate用法

---
+++ 笔记/enumerate函数用法.py
@@ -0,0 +1,13 @@
+#__author__ = 'tooweakchen'
+#coding:utf-8
+'''
+python enumerate函数用法
+enumerate 函数用于遍历序列中的元素以及它们的
+下标
+
+'''
+
+seq=['one','two','three']
+
+for i,element in enumerate(seq):
+    print i,element
